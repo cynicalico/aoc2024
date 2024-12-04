0e5182629a901cad44ab6655866fe3338e9d872d
cynicalico <93552084+cynicalico@users.noreply.github.com>
2024-12-04 15:32:27 -0700

    cleanup

--- src/day_4.rs
+++ src/day_4.rs
@@ -28,32 +28,27 @@ fn count_xmas(word_search: &Vec2D<char>, y: usize, x: usize) -> u32 {
     let mut n = 0;
 
     let word: Vec<char> = vec!['X', 'M', 'A', 'S'];
-    let deltas: Vec<(i32, i32)> = vec![
-        (0, -1),
-        (0, 1),
-        (-1, 0),
-        (1, 0),
-        (-1, -1),
-        (-1, 1),
-        (1, -1),
-        (1, 1),
-    ];
-
-    for (dy, dx) in deltas {
-        let mut found_word = true;
-        for i in 0..word.len() {
-            let pos = (
-                (y as i32 + (i as i32 * dy)) as usize,
-                (x as i32 + (i as i32 * dx)) as usize,
-            );
-
-            if *word_search.get(pos.0, pos.1).unwrap_or(&'\0') != word[i] {
-                found_word = false;
-                break;
+    for dy in -1..=1 {
+        for dx in -1..=1 {
+            if dy == 0 && dx == 0 {
+                continue;
             }
-        }
-        if found_word {
-            n += 1;
+
+            let mut found_word = true;
+            for i in 0..word.len() {
+                let pos = (
+                    (y as i32 + (i as i32 * dy)) as usize,
+                    (x as i32 + (i as i32 * dx)) as usize,
+                );
+
+                if *word_search.get(pos.0, pos.1).unwrap_or(&'\0') != word[i] {
+                    found_word = false;
+                    break;
+                }
+            }
+            if found_word {
+                n += 1;
+            }
         }
     }
 
@@ -78,25 +73,17 @@ fn calculate_p2_ans(word_search: &Vec2D<char>) -> u32 {
     p2_ans
 }
 
-#[rustfmt::skip]
 fn check_x_mas(word_search: &Vec2D<char>, y: usize, x: usize) -> bool {
+    let tl = word_search[(y - 1, x - 1)];
+    let bl = word_search[(y + 1, x - 1)];
+    let tr = word_search[(y - 1, x + 1)];
+    let br = word_search[(y + 1, x + 1)];
+
     // Only four possibilities, A is always anchored in the middle
-    (word_search[(y - 1, x - 1)] == 'M'
-        && word_search[(y + 1, x - 1)] == 'M'
-        && word_search[(y - 1, x + 1)] == 'S'
-        && word_search[(y + 1, x + 1)] == 'S') ||
-    (word_search[(y - 1, x - 1)] == 'S'
-        && word_search[(y + 1, x - 1)] == 'S'
-        && word_search[(y - 1, x + 1)] == 'M'
-        && word_search[(y + 1, x + 1)] == 'M') ||
-    (word_search[(y - 1, x - 1)] == 'M'
-        && word_search[(y + 1, x - 1)] == 'S'
-        && word_search[(y - 1, x + 1)] == 'M'
-        && word_search[(y + 1, x + 1)] == 'S') ||
-    (word_search[(y - 1, x - 1)] == 'S'
-        && word_search[(y + 1, x - 1)] == 'M'
-        && word_search[(y - 1, x + 1)] == 'S'
-        && word_search[(y + 1, x + 1)] == 'M')
+    (tl == 'M' && bl == 'M' && tr == 'S' && br == 'S')
+        || (tl == 'S' && bl == 'S' && tr == 'M' && br == 'M')
+        || (tl == 'M' && bl == 'S' && tr == 'M' && br == 'S')
+        || (tl == 'S' && bl == 'M' && tr == 'S' && br == 'M')
 }
 
 fn parse_puzzle_input() -> Vec2D<char> {
