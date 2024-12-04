932e986bc9a467c42fe94cdab8907655fa0c9cb3
cynicalico <93552084+cynicalico@users.noreply.github.com>
2024-12-04 16:52:15 -0700

    single regex for day 3

--- src/day_3.rs
+++ src/day_3.rs
@@ -51,42 +51,21 @@ fn calculate_p2_ans(instructions: &[Instruction]) -> i32 {
 }
 
 fn parse_puzzle_input() -> Vec<Instruction> {
-    let do_re = Regex::new(r"(do)\(\)").unwrap();
-    let dont_re = Regex::new(r"(don't)\(\)").unwrap();
-    let mul_re = Regex::new(r"(mul)\((\d{1,3}),(\d{1,3})\)").unwrap();
+    let re = Regex::new(r"(do|don't|mul)\((?:(\d{1,3}),(\d{1,3}))?\)").unwrap();
 
     let mut instructions = Vec::<Instruction>::new();
 
     for line in read_lines("input/day_3.txt").unwrap().flatten() {
-        let mut find_offset = 0;
-
-        loop {
-            let mut first_find: Option<(&Regex, usize)> = None;
-            for re in [&do_re, &dont_re, &mul_re] {
-                if let Some(m) = re.find_at(&line, find_offset) {
-                    if first_find.is_none() || m.start() < first_find.unwrap().1 {
-                        first_find = Some((re, m.start()));
-                    }
-                }
-            }
-
-            match first_find {
-                None => break,
-                Some((re, start)) => {
-                    let c = re.captures_at(&line, start).unwrap();
-                    find_offset = c.get(0).unwrap().end();
-
-                    instructions.push(match c.get(1).unwrap().as_str() {
-                        "do" => Instruction::Do,
-                        "don't" => Instruction::DoNot,
-                        "mul" => {
-                            let (_, [_, n, m]) = c.extract();
-                            Instruction::Mul(n.parse().unwrap(), m.parse().unwrap())
-                        }
-                        _ => unreachable!(),
-                    });
-                }
-            }
+        for c in re.captures_iter(&line) {
+            instructions.push(match c.get(1).unwrap().as_str() {
+                "do" => Instruction::Do,
+                "don't" => Instruction::DoNot,
+                "mul" => Instruction::Mul(
+                    c.get(2).unwrap().as_str().parse().unwrap(),
+                    c.get(3).unwrap().as_str().parse().unwrap(),
+                ),
+                _ => unreachable!(),
+            })
         }
     }
 
