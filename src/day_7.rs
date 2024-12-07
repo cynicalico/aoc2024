f7bb0cefc441a1461b6fdeef0c4ef974d922a13f
cynicalico <93552084+cynicalico@users.noreply.github.com>
2024-12-07 16:56:57 -0700

    better ergonomics + no more mut

--- src/day_7.rs
+++ src/day_7.rs
@@ -14,9 +14,9 @@ fn main() {
 
     let (p1_checked, p2_checked): (Vec<u64>, Vec<Option<u64>>) =
         equations.iter().partition_map(|(value, numbers)| {
-            match is_solvable(&p1_ops, *value, numbers[0], &numbers[1..]) {
+            match is_solvable(&p1_ops, *value, numbers) {
                 Some(v) => Either::Left(v),
-                None => Either::Right(is_solvable(&p2_ops, *value, numbers[0], &numbers[1..])),
+                None => Either::Right(is_solvable(&p2_ops, *value, numbers)),
             }
         });
 
@@ -34,29 +34,32 @@ enum Op {
     Cat,
 }
 
-fn is_solvable(ops: &[Op], v: u64, acc: u64, ns: &[u64]) -> Option<u64> {
-    if acc > v {
-        None
-    } else {
-        ops.iter()
-            .flat_map(|op| match op {
-                Op::Add => acc.checked_add(ns[0]),
-                Op::Mul => acc.checked_mul(ns[0]),
-                Op::Cat => {
-                    let mut n = ns[0];
-                    (0..=n.ilog10()).rev().try_fold(acc, |cat, i| {
-                        let d = n / 10u64.pow(i);
-                        n -= d * 10u64.pow(i);
-                        cat.checked_mul(10).map(|x| x + d)
-                    })
-                }
-            })
-            .any(|x| match &ns[1..] {
-                [] => x == v,
-                [rest @ ..] => is_solvable(ops, v, x, rest).is_some(),
-            })
-            .then_some(v)
+fn is_solvable(ops: &[Op], value: u64, numbers: &[u64]) -> Option<u64> {
+    fn inner(ops: &[Op], value: u64, numbers: &[u64], acc: u64) -> Option<u64> {
+        if acc > value {
+            None
+        } else {
+            ops.iter()
+                .flat_map(|op| match op {
+                    Op::Add => acc.checked_add(numbers[0]),
+                    Op::Mul => acc.checked_mul(numbers[0]),
+                    Op::Cat => (0..=numbers[0].ilog10())
+                        .rev()
+                        .try_fold((acc, numbers[0]), |(cat, n), i| {
+                            let d = n / 10u64.pow(i);
+                            cat.checked_mul(10).map(|x| (x + d, n - d * 10u64.pow(i)))
+                        })
+                        .map(|t| t.0),
+                })
+                .any(|x| match &numbers[1..] {
+                    [] => x == value,
+                    [rest @ ..] => inner(ops, value, rest, x).is_some(),
+                })
+                .then_some(value)
+        }
     }
+
+    inner(ops, value, &numbers[1..], numbers[0])
 }
 
 fn parse_puzzle_input() -> Vec<(u64, Vec<u64>)> {
