e1a7eed1482cfe66aff981493f29677ce55f6866
cynicalico <93552084+cynicalico@users.noreply.github.com>
2024-12-07 15:29:51 -0700

    modular design, easy to add new ops

--- src/day_7.rs
+++ src/day_7.rs
@@ -9,16 +9,19 @@ fn main() {
 
     let equations = parse_puzzle_input();
 
-    let (p1_checked, p2_checked): (Vec<Option<u64>>, Vec<Option<u64>>) =
+    let p1_ops = vec![Op::Add, Op::Mul];
+    let p2_ops = vec![Op::Add, Op::Mul, Op::Cat];
+
+    let (p1_checked, p2_checked): (Vec<u64>, Vec<Option<u64>>) =
         equations.iter().partition_map(|e| {
-            match is_solvable_p1(e.value, e.numbers[0], &e.numbers[1..]) {
-                None => Either::Right(is_solvable_p2(e.value, e.numbers[0], &e.numbers[1..])),
-                s => Either::Left(s),
+            match is_solvable(&p1_ops, e.value, e.numbers[0], &e.numbers[1..]) {
+                None => Either::Right(is_solvable(&p2_ops, e.value, e.numbers[0], &e.numbers[1..])),
+                Some(v) => Either::Left(v),
             }
         });
 
-    let p1_ans = p1_checked.iter().flatten().sum::<u64>();
-    let p2_ans = p1_ans + p2_checked.iter().flatten().sum::<u64>();
+    let p1_ans = p1_checked.into_iter().sum::<u64>();
+    let p2_ans = p1_ans + p2_checked.into_iter().flatten().sum::<u64>();
 
     println!("P1: {p1_ans}");
     println!("P2: {p2_ans}");
@@ -30,50 +33,38 @@ struct Equation {
     numbers: Vec<u64>,
 }
 
-fn is_solvable_p1(v: u64, acc: u64, ns: &[u64]) -> Option<u64> {
-    if acc > v {
-        None
-    } else {
-        match &ns {
-            [] => None,
-            [n] => (acc + n == v || acc * n == v).then_some(v),
-            [n, rest @ ..] => {
-                is_solvable_p1(v, acc + n, rest).or_else(|| is_solvable_p1(v, acc * n, rest))
-            }
-        }
-    }
+enum Op {
+    Add,
+    Mul,
+    Cat,
 }
 
-fn concatenate(mut n: u64, mut m: u64) -> Option<u64> {
-    for i in (0..=m.ilog10()).rev() {
-        let p = 10u64.pow(i);
-        let d = m / p;
-        match n.checked_mul(10) {
-            None => return None,
-            Some(v) => {
-                n = v;
-            }
-        }
-        n += d;
-        m -= d * p;
-    }
-    Some(n)
-}
-
-fn is_solvable_p2(value: u64, acc: u64, numbers: &[u64]) -> Option<u64> {
-    if acc > value {
+fn is_solvable(ops: &[Op], v: u64, acc: u64, ns: &[u64]) -> Option<u64> {
+    if acc > v || ns.is_empty() {
         None
     } else {
-        match &numbers {
-            [] => None,
-            [n] => {
-                (acc + n == value || acc * n == value || concatenate(acc, *n).unwrap_or(0) == value)
-                    .then_some(value)
-            }
-            [n, rest @ ..] => is_solvable_p2(value, acc + n, rest)
-                .or_else(|| is_solvable_p2(value, acc * n, rest))
-                .or_else(|| is_solvable_p2(value, concatenate(acc, *n).unwrap_or(0), rest)),
-        }
+        ops.iter()
+            .flat_map(|op| match op {
+                Op::Add => acc.checked_add(ns[0]),
+                Op::Mul => acc.checked_mul(ns[0]),
+                Op::Cat => {
+                    let mut n = ns[0];
+                    (0..=n.ilog10()).rev().try_fold(acc, |acc, i| {
+                        let p = 10u64.pow(i);
+                        let d = n / p;
+                        acc.checked_mul(10).and_then(|mut new_acc| {
+                            new_acc += d;
+                            n -= d * p;
+                            Some(new_acc)
+                        })
+                    })
+                }
+            })
+            .any(|x| match &ns[1..] {
+                [] => x == v,
+                [rest @ ..] => is_solvable(ops, v, x, rest).is_some(),
+            })
+            .then_some(v)
     }
 }
 
