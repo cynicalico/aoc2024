c5807b879c520b67aeca31ef8dc2bca1f822d3f0
cynicalico <93552084+cynicalico@users.noreply.github.com>
2024-12-07 14:24:04 -0700

    day 7 oom faster--not using strings to concatenate anymore

--- src/day_7.rs
+++ src/day_7.rs
@@ -9,20 +9,16 @@ fn main() {
 
     let equations = parse_puzzle_input();
 
-    let (p1_checked, p2_checked): (Vec<Option<i64>>, Vec<Option<i64>>) =
-        equations.iter().partition_map(|equation| {
-            match is_solvable_p1(&equation.value, equation.numbers[0], &equation.numbers[1..]) {
-                None => Either::Right(is_solvable_p2(
-                    &equation.value,
-                    equation.numbers[0],
-                    &equation.numbers[1..],
-                )),
+    let (p1_checked, p2_checked): (Vec<Option<u64>>, Vec<Option<u64>>) =
+        equations.iter().partition_map(|e| {
+            match is_solvable_p1(e.value, e.numbers[0], &e.numbers[1..]) {
+                None => Either::Right(is_solvable_p2(e.value, e.numbers[0], &e.numbers[1..])),
                 s => Either::Left(s),
             }
         });
 
-    let p1_ans = p1_checked.iter().flatten().sum::<i64>();
-    let p2_ans = p1_ans + p2_checked.iter().flatten().sum::<i64>();
+    let p1_ans = p1_checked.iter().flatten().sum::<u64>();
+    let p2_ans = p1_ans + p2_checked.iter().flatten().sum::<u64>();
 
     println!("P1: {p1_ans}");
     println!("P2: {p2_ans}");
@@ -30,32 +26,54 @@ fn main() {
 }
 
 struct Equation {
-    value: i64,
-    numbers: Vec<i64>,
+    value: u64,
+    numbers: Vec<u64>,
 }
 
-fn is_solvable_p1(value: &i64, acc: i64, numbers: &[i64]) -> Option<i64> {
-    match &numbers {
-        [] => None,
-        [n] => (acc + n == *value || acc * n == *value).then_some(*value),
-        [n, rest @ ..] => {
-            is_solvable_p1(value, acc + n, rest).or_else(|| is_solvable_p1(value, acc * n, rest))
+fn is_solvable_p1(v: u64, acc: u64, ns: &[u64]) -> Option<u64> {
+    if acc > v {
+        None
+    } else {
+        match &ns {
+            [] => None,
+            [n] => (acc + n == v || acc * n == v).then_some(v),
+            [n, rest @ ..] => {
+                is_solvable_p1(v, acc + n, rest).or_else(|| is_solvable_p1(v, acc * n, rest))
+            }
         }
     }
 }
 
-fn concatenate(n: i64, m: i64) -> i64 {
-    (n.to_string() + m.to_string().as_str()).parse().unwrap()
+fn concatenate(mut n: u64, mut m: u64) -> Option<u64> {
+    for i in (0..=m.ilog10()).rev() {
+        let p = 10u64.pow(i);
+        let d = m / p;
+        match n.checked_mul(10) {
+            None => return None,
+            Some(v) => {
+                n = v;
+            }
+        }
+        n += d;
+        m -= d * p;
+    }
+    Some(n)
 }
 
-fn is_solvable_p2(value: &i64, acc: i64, numbers: &[i64]) -> Option<i64> {
-    match &numbers {
-        [] => None,
-        [n] => (acc + n == *value || acc * n == *value || concatenate(acc, *n) == *value)
-            .then_some(*value),
-        [n, rest @ ..] => is_solvable_p2(value, acc + n, rest)
-            .or_else(|| is_solvable_p2(value, acc * n, rest))
-            .or_else(|| is_solvable_p2(value, concatenate(acc, *n), rest)),
+fn is_solvable_p2(value: u64, acc: u64, numbers: &[u64]) -> Option<u64> {
+    if acc > value {
+        None
+    } else {
+        match &numbers {
+            [] => None,
+            [n] => {
+                (acc + n == value || acc * n == value || concatenate(acc, *n).unwrap_or(0) == value)
+                    .then_some(value)
+            }
+            [n, rest @ ..] => is_solvable_p2(value, acc + n, rest)
+                .or_else(|| is_solvable_p2(value, acc * n, rest))
+                .or_else(|| is_solvable_p2(value, concatenate(acc, *n).unwrap_or(0), rest)),
+        }
     }
 }
 
