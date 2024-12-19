9f162e94295198371177966190c5a6910eaa7acd
cynicalico <93552084+cynicalico@users.noreply.github.com>
2024-12-19 13:19:06 -0700

    store max_key_len in trie directly, entry API is cool for building

--- src/util/trie.rs
+++ src/util/trie.rs
@@ -1,7 +1,7 @@
 use hashbrown::HashMap;
 
 struct Node {
-    pub children: HashMap<char, Box<Node>>,
+    pub children: HashMap<u8, Box<Node>>,
     pub is_terminal: bool,
 }
 
@@ -16,30 +16,30 @@ impl Node {
 
 pub struct Trie {
     root: Box<Node>,
+    pub max_key_len: usize,
 }
 
 impl Trie {
     pub fn new() -> Self {
         Self {
             root: Box::new(Node::new()),
+            max_key_len: 0,
         }
     }
 
     pub fn insert(&mut self, key: &str) {
         let mut curr = &mut self.root;
-        for ch in key.chars() {
-            if !curr.children.contains_key(&ch) {
-                curr.children.insert(ch, Box::new(Node::new()));
-            }
-            curr = curr.children.get_mut(&ch).unwrap();
+        for ch in key.as_bytes() {
+            curr = curr.children.entry(*ch).or_insert(Box::new(Node::new()));
         }
         curr.is_terminal = true;
+        self.max_key_len = self.max_key_len.max(key.len());
     }
 
     pub fn find(&self, key: &str) -> bool {
         let mut curr = &self.root;
-        for ch in key.chars() {
-            match curr.children.get(&ch) {
+        for ch in key.as_bytes() {
+            match curr.children.get(ch) {
                 None => return false,
                 Some(next) => curr = next,
             }
@@ -49,8 +49,8 @@ impl Trie {
 
     pub fn find_terminal(&self, key: &str) -> bool {
         let mut curr = &self.root;
-        for ch in key.chars() {
-            match curr.children.get(&ch) {
+        for ch in key.as_bytes() {
+            match curr.children.get(ch) {
                 None => return false,
                 Some(next) => curr = next,
             }
--- src/y2024/day19.rs
+++ src/y2024/day19.rs
@@ -3,84 +3,69 @@ use crate::util::trie::*;
 use hashbrown::HashMap;
 use std::io;
 
-type Input = (Trie, usize, Vec<String>);
+type Input = (Trie, Vec<String>);
 
 pub fn parse(filepath: &str) -> io::Result<Input> {
     let mut trie = Trie::new();
-    let mut max_pattern_len = 0;
     let mut designs = Vec::new();
 
     read_lines_partitioned(
         filepath,
         |line| {
-            line.split(", ").for_each(|pattern| {
-                max_pattern_len = max_pattern_len.max(pattern.len());
-                trie.insert(pattern);
-            });
+            line.split(", ").for_each(|pattern| trie.insert(pattern));
         },
         |line| {
             designs.push(line);
         },
     )?;
 
-    Ok((trie, max_pattern_len, designs))
+    Ok((trie, designs))
 }
 
 pub fn part1(input: &Input) -> Option<usize> {
-    let (trie, max_pattern_len, designs) = input;
+    let (trie, designs) = input;
     let mut memo: HashMap<String, bool> = HashMap::new();
     designs
         .iter()
-        .filter(|d| is_possible(&trie, d, *max_pattern_len, &mut memo))
+        .filter(|d| is_possible(&trie, d, &mut memo))
         .count()
         .into()
 }
 
 pub fn part2(input: &Input) -> Option<u64> {
-    let (trie, max_pattern_len, designs) = input;
+    let (trie, designs) = input;
     let mut memo: HashMap<String, u64> = HashMap::new();
     designs
         .iter()
-        .map(|d| count_possible(&trie, d, *max_pattern_len, &mut memo))
+        .map(|d| count_possible(&trie, d, &mut memo))
         .sum::<u64>()
         .into()
 }
 
-fn is_possible(
-    trie: &Trie,
-    d: &str,
-    max_pattern_len: usize,
-    memo: &mut HashMap<String, bool>,
-) -> bool {
-    let res = if let Some(b) = memo.get(d) {
+fn is_possible(trie: &Trie, d: &str, memo: &mut HashMap<String, bool>) -> bool {
+    if let Some(b) = memo.get(d) {
         *b
     } else {
-        d.len() <= max_pattern_len && trie.find_terminal(d)
-            || (1..d.len().min(max_pattern_len + 1)).rev().any(|i| {
-                trie.find_terminal(&d[..i]) && is_possible(trie, &d[i..], max_pattern_len, memo)
-            })
-    };
-    memo.insert(d.to_string(), res);
-    res
+        let res = d.len() <= trie.max_key_len && trie.find_terminal(d)
+            || (1..d.len().min(trie.max_key_len + 1))
+                .rev()
+                .any(|i| trie.find_terminal(&d[..i]) && is_possible(trie, &d[i..], memo));
+        memo.insert(d.to_string(), res);
+        res
+    }
 }
 
-fn count_possible(
-    trie: &Trie,
-    d: &str,
-    max_pattern_len: usize,
-    memo: &mut HashMap<String, u64>,
-) -> u64 {
-    let res = if let Some(c) = memo.get(d) {
+fn count_possible(trie: &Trie, d: &str, memo: &mut HashMap<String, u64>) -> u64 {
+    if let Some(c) = memo.get(d) {
         *c
     } else {
         let mut total = if trie.find_terminal(d) { 1 } else { 0 };
-        for i in 1..d.len().min(max_pattern_len + 1) {
+        for i in (1..d.len().min(trie.max_key_len + 1)).rev() {
             if trie.find_terminal(&d[..i]) {
-                total += count_possible(trie, &d[i..], max_pattern_len, memo);
+                total += count_possible(trie, &d[i..], memo);
             }
         }
+        memo.insert(d.to_string(), total);
         total
-    };
-    memo.insert(d.to_string(), res);
-    res
+    }
 }
